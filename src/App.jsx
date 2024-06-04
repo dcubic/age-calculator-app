644b43c9d2713d400ee3302548c432b9d6d7fe5e
unknown <dominikcubic@gmail.com>
2024-06-03 19:04:53 -0700

    :construction: building code-first this time

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,99 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styles from "./App.module.css";
+import { useState } from "react";
+import IconArrow from "./assets/images/icon-arrow.svg?react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [enteredValues, setEnteredValues] = useState({
+    day: '',
+    month: '',
+    year: '',
+  });
+
+  const [hasEditted, setHasEditted] = useState({
+    day: false,
+    month: false,
+    year: false
+  });
+
+  const isDayInvalid = hasEditted.day && (1 > +enteredValues.day || +enteredValues.day > 31);
+  const isMonthInvalid = hasEditted.month && (+enteredValues.month < 1 || 12 < +enteredValues.month);
+
+  function onSubmit(event) {
+    event.preventDefault();
+    console.log("Button clicked!");
+    console.log("values: " + JSON.stringify(enteredValues));
+    console.log("editted: " + JSON.stringify(hasEditted));
+  }
+
+  function handleInputChange(identifier, newValue) {
+    setEnteredValues((previousValues) => ({
+      ...previousValues,
+      [identifier]: newValue,
+    }));
+  }
+
+  function handleInputBlur(identifier) {
+    setHasEditted(previousState => ({
+      ...previousState,
+      [identifier] : true
+    }));
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <form className={styles.form} onSubmit={onSubmit}>
+        <div className={styles.inputsContainer}>
+          <div className={styles.inputContainer}>
+            <label htmlFor="day-input">Day</label>
+            <input
+              id="day-input"
+              placeholder="DD"
+              onBlur={() => handleInputBlur('day')}
+              onChange={(event) => handleInputChange('day', event.target.value)}
+              value={enteredValues.day}
+            ></input>
+            {isDayInvalid ? <p>Must be a valid day</p>: <></>}
+          </div>
+          <div className={styles.inputContainer}>
+            <label htmlFor="month-input">Month</label>
+            <input
+              id="month-input"
+              placeholder="MM"
+              onBlur={() => handleInputBlur('month')}
+              onChange={(event) => handleInputChange('month', event.target.value)}
+              value={enteredValues.month}
+            ></input>
+            {isMonthInvalid ? <p>Must be a valid month</p> : <></>}
+          </div>
+          <div className={styles.inputContainer}>
+            <label htmlFor="year-input">Year</label>
+            <input id="year-input" placeholder="YYYY" type="number"></input>
+            <div>Error State Here!</div>
+          </div>
+        </div>
+        <div className={styles.dividerButtonContainer}>
+          <hr className={styles.lineBreak}></hr>
+          <button className={styles.submitButton}>
+            <IconArrow></IconArrow>
+          </button>
+        </div>
+      </form>
+      <div className={styles.outputsContainer}>
+        <div>
+          <div className={styles.outputValue}>TODO: years output</div>
+          <div className={styles.units}>years</div>
+        </div>
+        <div>
+          <div className={styles.outputValue}>TODO</div>
+          <div className={styles.units}>months</div>
+        </div>
+        <div>
+          <div className={styles.outputValue}>TODO</div>
+          <div className={styles.units}>days</div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
